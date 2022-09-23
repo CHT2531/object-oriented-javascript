7768ab9107422c4d62535b645fa284a7b13af325
mmantle-hud <m.e.mantle@hud.ac.uk>
2022-09-23 11:54:54 +0100

    Update oo-js.js

--- js/oo-js.js
+++ js/oo-js.js
@@ -36,26 +36,43 @@ e) Output the age of the film (you will  need to call the getAge() method).
 f) Using a forEach loop output the entire cast.
 */
 
-//Q2. Create an object literal to represent a cake. The object should have properties for type (a string), layers (a number), and ingredients (an array of strings). Assign the newly created object to a variable named myCake. Here's some code that tests it works.
+//Q2. Create an object literal to represent a cake. 
+//The object should have properties for type (a string), layers (a number), and ingredients (an array of strings). 
+// Assign the newly created object to a variable named myCake. 
+// Uncomment the following code to tests this works.
 // console.log(myCake.type) // e.g. Sponge
 // console.log(myCake.layers) // e.g. 2 
 // console.log(myCake.ingredients) // e.g. ["flour","sugar","butter","eggs"]
 
-//Q2. Add a bake()  method to the object. bake() should return a string using the type value. Again, here's some code to tests it works.
+//Q2. Add a bake()  method to the object. bake() should return a string using the type value. 
+// Again, here's some code to tests it works.
 // console.log(myCake.bake()) // e.g. The sponge cake is baking.
 
-//Q3. Make a new object and call it cakePrinter. cakePrinter should feature a single method printDetails(). The method should accept a single argument, a cake object. The printDetails() method should print out the details of the cake in the console.log e.g.
-//cakePrinter.printDetails(myCake); //Type:Sponge Layers:2 Ingredients: flour sugar butter eggs
-
-//Q4. Create a factory function that will create instances of cake objects. The factory function should be named cake, it should accept three arguments and return an object featuring the same properties and methods as in Q1. 
-// var chocCake = cake("Chocolate",3,["flour","sugar","chocolate","eggs"]);
-// var fruitCake = cake("Fruit",2,["flour","sugar","fruit","eggs"]);
+//Q3. Create a factory function that will create instances of cake objects. 
+//The factory function should be named cake, it should accept three arguments and return a cake object. 
+// Again, uncomment the following to test your code
+// const chocCake = cakeFactory("Chocolate", 3, ["flour", "sugar", "chocolate", "eggs"]);
+// const fruitCake = cakeFactory("Fruit", 2, ["flour", "sugar", "sultanas", "eggs"]);
 // console.log(chocCake.bake()); // The Chocolate cake is baking. 
-// cakePrinter.printDetails(chocCake); //prints full details 
-// cakePrinter.printDetails(fruitCake); //prints full details 
+// console.log(`The ${fruitCake.type} has ${fruitCake.layers} layers.`); // The Fruit cake has 2 layers.
 
+//Q4. Comment out the previous code. 
+// Re-write this program to use a constructor function. Here are the tests:
+// const chocCake = new Cake("Chocolate", 3, ["flour", "sugar", "chocolate", "eggs"]);
+// const fruitCake = new Cake("Fruit", 2, ["flour", "sugar", "sultanas", "eggs"]);
+// console.log(fruitCake.bake()); // The Fruit cake is baking. 
+// console.log(`The ${chocCake.type} has ${chocCake.layers} layers.`); // The Chocolate cake has 3 layers.
+
+//Q5. Comment out the previous code.
+// Re-write the program to use ES2015 classes
+// You can use the same tests as in Q4
+
+//Q6. Write a series of console.log() statements that will output a complete description of the cake i.e. the name, no. of layers and all the ingredients.
+// Make sure that you use a forEach loop to display the list of ingredients. 
+
+//Q7. Create at least three cake objects and store them in an array
 //Q5. Modify your factory function so that it accepts an object literal to specify values for the object properties and return an object featuring the same properties and methods as above. Again, here's an example:
-//var fruitCake=cake({type:"Fruit",layers:2,ingredients:["flour","sugar","fruit","eggs"]})
+//var fruitCake=cake({type:"Fruit",layers:2,ingredients:["flour","sugar","sultanas","eggs"]})
 //cakePrinter.printDetails(fruitCake);
 
 //Q5. Modify the above so that default values are used for name, layers and ingredients if any of these properties aren't specified. again here's an example of the function being called. 
@@ -65,5 +82,7 @@ f) Using a forEach loop output the entire cast.
 // Q6. Modify the  printDetails method of the cakePrinter() so that it also accepts a CSS selector for an HTML element. Instead of printing the details in the console.log it should output the details in the HTML page. 
 //cakePrinter.printDetails(myCake,"#output");
 
+//Q3. Make a new object and call it cakePrinter. cakePrinter should feature a single method printDetails(). The method should accept a single argument, a cake object. The printDetails() method should print out the details of the cake in the console.log e.g.
+//cakePrinter.printDetails(myCake); //Type:Sponge Layers:2 Ingredients: flour sugar butter eggs
 
 
