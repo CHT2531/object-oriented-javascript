a61d7ea2905cb37785be308bda4c3563a637d248
mmantle-hud <m.e.mantle@hud.ac.uk>
2022-10-04 10:22:59 +0100

    Update oo-js.js

--- js/oo-js.js
+++ js/oo-js.js
@@ -1,5 +1,49 @@
+//Q1. Have a look at the following object literal.
+const flight = {
+    flightNumber: 2131,
+    date: "2023-04-10",
+    airline: {
+        code: "FR",
+        name: "Ryanair"
+    },
+    departure: {
+        code: "MAN",
+        name: "Manchester"
+    },
+    arrival: {
+        code: "ACE",
+        name: "Lanzarote"
+    }
+}
 
-//Q1. Have a look at the following object literal. 
+// Using this object literal, write console.log statements that will
+// a) Output the flightNumber.
+// b) Output the date. 
+// c) Output the name of the arrival airport.
+// d) Output the sentence 'Flight 2131 to Lanzarote will be departing from Manchester on 2023-04-10'
+
+
+//Q2. Have a look at the following arrays of objects. 
+//These flight objects have exactly the same properties as in Q1, I've just removed the spacing and line breaks.
+
+const flights = [
+    {flightNumber: 2131, date:"2023-04-10", airline: {code: "FR", name: "Ryanair"}, departure:{code: "MAN", name: "Manchester"}, arrival:{code: "ACE",name: "Lanzarote"}},
+    {flightNumber: 318,date: "2023-03-05",airline: {code: "AF",name: "Air France"},departure:{code: "MAN",name: "Manchester"},arrival:{code: "CDG",name: "Paris"}},
+    {flightNumber: 217,date: "2023-02-12", airline: {code: "LS",name: "Jet2"},departure:{code: "LBA",name: "Leeds"},arrival:{code: "MAH",name: "Menorca"}}
+]
+
+//Using this array,, write console.log statements that will
+//a) Using a forEach loop output the flight numbers i.e.
+// 2131
+// 318
+// 217
+//b) Output the airline code, flight number, date and airline name for each flight i.e.
+// FR 2131 2023-04-10 Ryanair
+// AF 318 2023-03-05 Air France
+// LS 217 2023-02-12 Jet2
+//c) Can you filter out the flights that depart from Manchester. Use a forEach to display these filtered flights.
+
+//Q3. Have a look at the following object literal. 
 
 const filmObj = {
     title: "Jaws",
@@ -36,7 +80,7 @@ e) Output the age of the film (you will  need to call the getAge() method).
 f) Using a forEach loop output the entire cast.
 */
 
-//Q2. Create an object literal to represent a cake. 
+//Q4. Create an object literal to represent a cake. 
 //The object should have properties for type (a string), layers (a number), and ingredients (an array of strings). 
 // Assign the newly created object to a variable named myCake. 
 // Uncomment the following code to tests this works.
@@ -44,11 +88,11 @@ f) Using a forEach loop output the entire cast.
 // console.log(myCake.layers) // e.g. 2 
 // console.log(myCake.ingredients) // e.g. ["flour","sugar","butter","eggs"]
 
-//Q2. Add a bake()  method to the object. bake() should return a string using the type value. 
+//Q5. Add a bake()  method to the object. bake() should return a string using the type value. 
 // Again, here's some code to tests it works.
 // console.log(myCake.bake()) // e.g. The sponge cake is baking.
 
-//Q3. Create a factory function that will create instances of cake objects. 
+//Q6. Create a factory function that will create instances of cake objects. 
 //The factory function should be named cake, it should accept three arguments and return a cake object. 
 // Again, uncomment the following to test your code
 // const chocCake = cakeFactory("Chocolate", 3, ["flour", "sugar", "chocolate", "eggs"]);
@@ -56,33 +100,16 @@ f) Using a forEach loop output the entire cast.
 // console.log(chocCake.bake()); // The Chocolate cake is baking. 
 // console.log(`The ${fruitCake.type} has ${fruitCake.layers} layers.`); // The Fruit cake has 2 layers.
 
-//Q4. Comment out the previous code. 
+//Q7. Comment out the previous code. 
 // Re-write this program to use a constructor function. Here are the tests:
 // const chocCake = new Cake("Chocolate", 3, ["flour", "sugar", "chocolate", "eggs"]);
 // const fruitCake = new Cake("Fruit", 2, ["flour", "sugar", "sultanas", "eggs"]);
 // console.log(fruitCake.bake()); // The Fruit cake is baking. 
 // console.log(`The ${chocCake.type} has ${chocCake.layers} layers.`); // The Chocolate cake has 3 layers.
 
-//Q5. Comment out the previous code.
+//Q8. Comment out the previous code.
 // Re-write the program to use ES2015 classes
-// You can use the same tests as in Q4
+// You can use the same tests as in Q7
 
-//Q6. Write a series of console.log() statements that will output a complete description of the cake i.e. the name, no. of layers and all the ingredients.
-// Make sure that you use a forEach loop to display the list of ingredients. 
-
-//Q7. Create at least three cake objects and store them in an array
-//Q5. Modify your factory function so that it accepts an object literal to specify values for the object properties and return an object featuring the same properties and methods as above. Again, here's an example:
-//var fruitCake=cake({type:"Fruit",layers:2,ingredients:["flour","sugar","sultanas","eggs"]})
-//cakePrinter.printDetails(fruitCake);
-
-//Q5. Modify the above so that default values are used for name, layers and ingredients if any of these properties aren't specified. again here's an example of the function being called. 
-//var someCake=cake({layers:2,ingredients:["flour","sugar","cherries","eggs"]})
-//cakePrinter.printDetails(someCake); //still print the full detail but uses default value for the type of cake
-
-// Q6. Modify the  printDetails method of the cakePrinter() so that it also accepts a CSS selector for an HTML element. Instead of printing the details in the console.log it should output the details in the HTML page. 
-//cakePrinter.printDetails(myCake,"#output");
-
-//Q3. Make a new object and call it cakePrinter. cakePrinter should feature a single method printDetails(). The method should accept a single argument, a cake object. The printDetails() method should print out the details of the cake in the console.log e.g.
-//cakePrinter.printDetails(myCake); //Type:Sponge Layers:2 Ingredients: flour sugar butter eggs
 
 
